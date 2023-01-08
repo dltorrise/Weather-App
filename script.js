54314cbfda2e18f78c13e3028e23839b7df7d5d8
dltorrise <dltorrise@gmail.com>
2023-01-08 15:17:09 -0600

    current weather

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ var getWeatherBtn = document.getElementById("user-form")
 var cityInput = document.getElementById("city")
 //template literal so you dont have to concatenate
 //var fiveDayUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`
+var resultsContainer = document.getElementById("results")
+var fiveDayContainer = document.getElementById("five-day")
+// var latitude
+// var longitude
+console.log(resultsContainer)
 
 
 console.log('Hello')
@@ -16,10 +21,51 @@ function getCurrentWeather(city) {
         return response.json()
     
     }) .then(function (data) {
+        //city name, the date, an icon representation of weather conditions, 
+        //the temperature, the humidity, and the wind speed
         console.log(data)
-    })
-}
+            //Creating a h3 element and a p element
+                var cityName = document.createElement('h3');
+                var icon = document.createElement('img');
+                icon.classList.add("icon") //add icon class
+                var temperature = document.createElement('p');
+                var humidity = document.createElement('p');
+                var windSpeed = document.createElement('p');
+    
+            //Setting the text of the h3 element and p element.
+                cityName.textContent = data.name
+                iconName = data.weather.icon
+                //icon.src = `https://openweathermap.org/img/wn/${iconName}@2x.png`;
+                temperature.textContent = data.main.temp;
+                windSpeed.textContent = data.wind.speed
+                humidity.textContent = data.main.humidity
 
+                console.log(temperature);
+   
+            //Appending the dynamically generated html to the div associated with the id="users"
+            //Append will attach the element as the bottom most child.
+                resultsContainer.append("City Name: " + city);
+                resultsContainer.append(icon)
+                resultsContainer.append("Temperature: ")
+                resultsContainer.append(temperature)
+                resultsContainer.append("Wind Speed: ")
+                resultsContainer.append(windSpeed)
+                resultsContainer.append("Humidity: ")
+                resultsContainer.append(humidity)
+        });
+};
+
+
+// function getFutureWeather(city) {
+//     fetch(fiveDayUrl) //returns response
+//     .then(function (response) {
+//         return response.json()
+    
+//     }) .then(function (data) {
+//         console.log(data)
+                
+//     });
+// };
 
 function handleSearchSubmit(event) {
     console.log('clicked')
@@ -30,6 +76,7 @@ function handleSearchSubmit(event) {
         var city = cityInput.value.trim() //searchInput is an HTML element and so .value is input
         //.trim bc white space can sometimes mess up databases
         getCurrentWeather(city) //calls function as city as parameter
+        getFutureWeather(ciy)
         cityInput.value = '' //clears search box by creating empty string
     }
 }
